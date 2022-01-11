d68f1bca78ddf083daff3b5a218cc813524e7ca4
13400520 <a13400520@MAC13400520.local>
2022-01-11 13:49:43 +0530

    Custome authentication filter integrated and commented.

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/authentication/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package com.in28minutes.rest.webservices.restfulwebservices.security.authentication.filter;
+
+import com.in28minutes.rest.webservices.restfulwebservices.security.authentication.CustomAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@Component
+public class CustomAuthenticationFilter implements Filter {
+
+    @Autowired
+    private AuthenticationManager customAuthenticationManager;
+    @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        var httpRequest = (HttpServletRequest) request;
+        var httpResponse = (HttpServletResponse) response;
+        String authorization  = httpRequest.getHeader("Authorization");
+        CustomAuthentication customAuthentication = new CustomAuthentication(authorization, null);
+       try {
+           Authentication result = customAuthenticationManager.authenticate(customAuthentication);
+
+           if (result.isAuthenticated()) {
+               SecurityContextHolder.getContext().setAuthentication(result);
+               chain.doFilter(request, response);
+           }else
+               httpResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+       }catch(AuthenticationException e){
+           httpResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+       }
+    }
+}
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/config/UserDetailServiceConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/config/UserDetailServiceConfig.java
@@ -1,7 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices.security.config;
 
+import com.in28minutes.rest.webservices.restfulwebservices.security.authentication.filter.CustomAuthenticationFilter;
+import com.in28minutes.rest.webservices.restfulwebservices.security.authentication.providers.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -11,50 +16,78 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class UserDetailServiceConfig extends WebSecurityConfigurerAdapter {
 
-	/*
-	 * @Autowired private DataSource dataSource;
-	 */
+    /*
+     * @Autowired private DataSource dataSource;
+     */
 
-	/**
-	 * In memory implementation
-	 * 
-	 * @return
-	 */
-	// @Bean
-	public UserDetailsService userDetailsService() {
-		UserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-		UserDetails userDetails = User.builder().username("bill").password("12345").authorities("read").build();
-		userDetailsService.createUser(userDetails);
-		return userDetailsService;
-	}
+    //@Autowired
+    //private CustomAuthenticationFilter filter;
 
-	/**
-	 * JDBC implementation used for creating the user and modifying the users it
-	 * also helps u to get validate the users by username and password
-	 * 
-	 * @return
-	 */
+    //@Autowired
+    //private CustomAuthenticationProvider customAuthenticationProvider;
 
-	/*
-	 * @Bean("userDetailsManager") public UserDetailsManager
-	 * userDetailsServiceJdbc() { return new JdbcUserDetailsManager(dataSource); }
-	 */
-	// UserDetails }
+    /**
+     * In memory implementation
+     *
+     * @return
+     */
+    // @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails userDetails = User.builder().username("bill").password("12345").authorities("read").build();
+        userDetailsService.createUser(userDetails);
+        return userDetailsService;
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		// return NoOpPasswordEncoder.getInstance();
-		return new BCryptPasswordEncoder();
-	}
+    /**
+     * JDBC implementation used for creating the user and modifying the users it
+     * also helps u to get validate the users by username and password
+     *
+     * @return
+     */
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic();
-		http.csrf().disable().authorizeRequests().mvcMatchers("/user").permitAll().anyRequest().authenticated();
-	}
+    /*
+     * @Bean("userDetailsManager") public UserDetailsManager
+     * userDetailsServiceJdbc() { return new JdbcUserDetailsManager(dataSource); }
+     */
+    // UserDetails }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.csrf().disable().authorizeRequests().mvcMatchers("/user").permitAll().anyRequest().authenticated();
+    }
+
+    /**@Override
+    @Bean("customAuthenticationManager")
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }*/
+
+    /**
+     * user Custom authentication filter
+     * @param auth
+     * @throws Exception
+     */
+   /** @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(customAuthenticationProvider);
+    }*/
+
+/**@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterAt(filter, BasicAuthenticationFilter.class);
+
+        http.authorizeRequests().anyRequest().permitAll();
+    }*/
 }
